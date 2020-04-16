3472d5bddb5d92b0cdcdaef234e885cd797678b5
Kirill Moizik <moizik@gmail.com>
2020-04-15 22:40:15 -0400

    deposit works

--- misc/scripts/start.sh
+++ misc/scripts/start.sh
@@ -1,3 +1,4 @@
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml  down;
 docker-compose  -f docker-compose.yml -f docker-compose.dev.yml up -d db redis mysql ghost ganache;
 cd ./ethereumContracts;
 npx truffle deploy --network development;
