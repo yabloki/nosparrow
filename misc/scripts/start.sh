ad99a39bd96cb7b4f3bc23d9ba7270fcf5d7a7c1
Kirill Moizik <moizik@gmail.com>
2020-05-08 23:26:24 -0400

    rb works

--- misc/scripts/start.sh
+++ misc/scripts/start.sh
@@ -4,4 +4,4 @@ cd ./ethereumContracts;
 npx truffle deploy --network development;
 cd ..;
 docker build -t coreserver -f coreserver/Dockerfile  .;
-docker-compose  -f docker-compose.yml -f docker-compose.dev.yml up ofelia coreserver;
+docker-compose  -f docker-compose.yml -f docker-compose.dev.yml up ofelia coreserver eventProcessor;
