a6192269e8ad35575e00288eef811bf510df348e
Kirill Moizik <moizik@gmail.com>
2020-05-24 14:10:34 -0400

    invites

--- misc/scripts/start.sh
+++ misc/scripts/start.sh
@@ -1,11 +1,17 @@
-cd Ghost;
-grunt release;
-docker build -t ghost:custom .;
-cd ../;
+#!/bin/bash
+if [ "$1" = "-b" ]; then
+    cd Ghost;
+    grunt release;
+    docker build -t ghost:custom .;
+    cd ../;
+fi
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml  down;
 docker-compose  -f docker-compose.yml -f docker-compose.dev.yml up -d db redis mysql ghost ganache;
+sleep 2;
 cd ./ethereumContracts;
 npx truffle deploy --network development;
 cd ..;
-docker build -t coreserver -f coreserver/Dockerfile  .;
+if [ "$1" = "-b" ]; then
+    docker build -t coreserver -f coreserver/Dockerfile  .;
+fi
 docker-compose  -f docker-compose.yml -f docker-compose.dev.yml up ofelia coreserver eventProcessor;
